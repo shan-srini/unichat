2d5976f2bdee9f7826c34f9ca3b2dcb76d2b7791
ValentiJ <valenti.j@northeastern.edu>
2021-06-16 00:03:39 -0400

    Update Messenger.jsx

--- client/src/components/Messenger.jsx
+++ client/src/components/Messenger.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import Conversation from './Conversation';
 import Groups from './Groups';
+import User from "./User"
 import styled from 'styled-components';
 import Divider from '@material-ui/core/Divider';
 
@@ -12,9 +13,16 @@ const Container = styled.div`
   min-height: 0;
 `;
 
+const GroupHeader = styled.h1`
+  margin: 2.5% 200px;
+  color: #49A2F4;
+`;
+
 export default function Messenger() {
   const [socket, setSocket] = useState(null);
   const [groups, setGroups] = useState([]);
+  const [user, setUser] = useState("");
+  const [lang, setLang] = useState("");
   //TODO:  should really start as empty, should have both name and id
   const [currGroup, setCurrGroup] = useState(0);
 
@@ -28,16 +36,46 @@ export default function Messenger() {
     setCurrGroup(groups.length);
   }
 
+  const onSetUser = (u) => {
+    setUser([u]);
+  }
+
+  const onSetUserLang = (l) => {
+    setLang([l]);
+  }
+
   return (
     <Container>
+        {user === "" ? (
+        <User
+        username={user}
+        userLang={lang}
+        onSetUser={onSetUser}
+        onSetUserLang={onSetUserLang}
+        
+        />
+
+      ) : (
       <Groups
         groups={groups}
         selectedGrpIdx={currGroup}
         onAddGroup={onAddGroup}
         onSelectGroup={setCurrGroup}
-      />
+      />)}
+
+
+      {user === "" ? (
+        <></>
+      ) : (
       <Divider orientation="vertical" flexItem />
-      <Conversation group={groups[currGroup]} socket={socket} />
+          )}
+
+      {user === "" ? (
+        <></>
+      ) : (
+      <Conversation group={groups[currGroup]} socket={socket}/>
+        )}
+
     </Container>
   );
 }
